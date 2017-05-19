22248686d79294e1870dc1b1406a0e18eaaf67cf
mayonklepas <imonks21@gmail.com>
2017-05-19 11:54:12 +0800

    adding login control

--- app/src/main/java/com/alazharbsd/masjid/masjidapp/LoginActivity.java
+++ app/src/main/java/com/alazharbsd/masjid/masjidapp/LoginActivity.java
@@ -2,12 +2,24 @@ package com.alazharbsd.masjid.masjidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email,password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email=(EditText) findViewById(R.id.email);
+        password=(EditText) findViewById(R.id.password);
+        login=(Button) findViewById(R.id.login);
+    }
+
+
+    private void login(){
+
     }
 }
