a09433178c0ac154e726feec9f047d7719ffcffb
mayonklepas <imonks21@gmail.com>
2017-05-24 12:39:46 +0800

    finaizing update v4

--- app/src/main/java/com/alazharbsd/masjid/masjidapp/Adaptervideo.java
+++ app/src/main/java/com/alazharbsd/masjid/masjidapp/Adaptervideo.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -99,7 +100,7 @@ public class Adaptervideo extends RecyclerView.Adapter<Adaptervideo.Holder>{
 
                     @Override
                     public void onThumbnailError(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader.ErrorReason errorReason) {
-
+                        Toast.makeText(ct, errorReason.toString(), Toast.LENGTH_SHORT).show();
                     }
                 });
             }
